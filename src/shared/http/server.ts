25fc7d385f5661f3c6b56812c0bfc1f16295cf76
CaparicaLeo <leo_caparica@icloud.com>
2025-05-19 20:51:39 -0300

    creating AppError

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,33 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import routes from "./routes";
 import cors from "cors";
+import AppError from "@shared/errors/AppError";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use(
+	(
+		error: Error,
+		request: Request,
+		response: Response,
+		next: NextFunction
+	) => {
+		if (error instanceof AppError) {
+			response.status(error.statusCode).json({
+				status: "error",
+				message: error.message,
+			});
+		}
+		response.status(500).json({
+			status: "error",
+			message: "Internal server error",
+		});
+	}
+);
+
 app.listen(3333, () => {
 	console.log("Server started on port 3333!");
 });
